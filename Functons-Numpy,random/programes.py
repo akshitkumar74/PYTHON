4839464a3ecf8370218f5268be10b9f5ffe6eaa1
akshitkumar74 <95975306+akshitkumar74@users.noreply.github.com>
2022-05-25 18:57:37 +0530

    Add files via upload

---
+++ Functons-Numpy,random/programes.py
@@ -0,0 +1,56 @@
+'''import numpy as np
+a=np.array([1,2,3,4,5,6])
+b=np.array([[1,2],[2,3],[3,4]])
+print(a.ndim)
+print(a.shape)
+print(b.shape)
+b=b.flatten()  
+a=a.reshape(3,2) 
+c=np.eye(5)  
+print(a)
+print(b)
+print
+import numpy as np
+a=np.array(eval(input()))'''
+
+
+'''import numpy as np
+import random
+l=[]
+for i in range(5):
+    a=random.random()
+    l.append(a)
+    a1=np.array(l)
+print(a1)
+import numpy as np
+a=np.random.rand(5)
+print(a)'''
+
+
+
+'''import random
+a=random.randint(0,9)
+print(a)'''
+
+
+'''import random
+a=random.randint(1000,9999)
+print(a)'''
+
+
+'''import random
+a=random.randrange(0,9)
+print(a)'''
+
+
+'''import random
+a=random.randint(1000,9999)
+for i in range(10):
+    print(a)'''
+    
+    
+    
+'''import random
+l=['mango','orange','apple']
+random.shuffle(l)
+print(l)'''
